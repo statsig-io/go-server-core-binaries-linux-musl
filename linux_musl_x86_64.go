50d4bcd80ff09521bb64c26c8adc1d0f27af7df5
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2025-10-06 17:27:18 -0700

    chore: add binaries for 0.9.3

---
+++ linux_musl_x86_64.go
@@ -0,0 +1,21 @@
+//go:build linux && musl && x86_64
+
+package go_server_core_binaries_linux_musl
+
+import (
+	_ "embed"
+)
+
+//go:embed linux_musl_x86_64.so
+var binaryData []byte
+
+//go:embed linux_musl_x86_64.so.sig
+var signatureData []byte
+
+func GetBinaryData() []byte {
+	return binaryData
+}
+
+func GetSignatureData() []byte {
+	return signatureData
+}
